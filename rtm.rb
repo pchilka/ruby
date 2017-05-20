fd5eb3175ecad0c23791164fc8fc215d14f159c8
Pradeep Chilka <pchilka@gmail.com>
2017-05-20 08:45:56 -0400

    fix space before description

--- rtm.rb
+++ rtm.rb
@@ -8,13 +8,12 @@ Dir.foreach(FDIR) do |file|
         #print "Feature file: #{file}\n";
         filepath = FDIR + "/#{file}";
         File.open(filepath).each do |line|
-            line = line.chomp
             if line =~ /^\s*Scenario/ 
-                if line =~ /^\s*Scenario\s*:?(.*?)(\d+\.\d+\.\d+)(.*$)/ 
+                if line =~ /^\s*Scenario\s*:?\s*(.*?)(\d+\.\d+\.\d+)(.*$)/ 
                     print $2 + "\t";
                     print "Scenario: " + $1 + $2 + $3 + "\t";
                     print file + "\n";
-                elsif line =~ /^\s*Scenario\s*:?(.*)$/ 
+                elsif line =~ /^\s*Scenario\s*:\s*?(.*)$/ 
                     print "" + "\t";
                     print "Scenario: " + $1 + "\t";
                     print file + "\n";
