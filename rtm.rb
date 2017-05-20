88806426be35c375037adfeee587595fa64dea6f
Pradeep Chilka <pchilka@gmail.com>
2017-05-19 21:37:00 -0400

    added reading all feature files

--- rtm.rb
+++ rtm.rb
@@ -2,14 +2,22 @@
 
 rtm = Hash.new(0);
 
-File.open("Feature.txt").each do |line|
-	if line =~ /Scenario/ 
-		print line;
-		if line =~ /Scenario:.*?(\d+\.\d+\.\d+)\.\d+.*$/ 
-			print "Extracted>>>>>>> " + $1 + "\n";
-			rtm[$1] += 1;
+FDIR = "features";
+
+Dir.foreach(FDIR) do |file|
+		if file =~ /\.feature$/
+				print "Feature file: #{file}\n";
+				filepath = FDIR + "/#{file}";
+				File.open(filepath).each do |line|
+					if line =~ /Scenario/ 
+						print line;
+						if line =~ /Scenario:.*?(\d+\.\d+\.\d+)\.\d+.*$/ 
+							print "Extracted>>>>>>> " + $1 + "\n";
+							rtm[$1] += 1;
+						end
+					end
+				end
 		end
-	end
 end
 
 rtm.each {|key, val| print "req=#{key}, count=#{val}\n";}
