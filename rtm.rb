d5b27c7d6da3f8f6f0e21a19a4e1aad11aa3ddc9
Pradeep Chilka <pchilka@gmail.com>
2017-05-19 21:03:52 -0400

    added hash

--- rtm.rb
+++ rtm.rb
@@ -1,10 +1,15 @@
 # RTM
 
+rtm = Hash.new;
+
 File.open("Feature.txt").each do |line|
 	if line =~ /Scenario/ 
 		print line;
 		if line =~ /Scenario:.*?(\d+\.\d+\.\d+)\.\d+.*$/ 
 			print "Extracted>>>>>>> " + $1 + "\n";
+			rtm[$1] = rtm[$1].nil? ? 0 : rtm[$1]+1
 		end
 	end
 end
+
+rtm.each {|key, val| print "req=#{key}, count=#{val}\n";}
