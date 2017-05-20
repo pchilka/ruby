6c5bbf7a8dc60d336e4d659b5fb271c1b256f593
Pradeep Chilka <pchilka@gmail.com>
2017-05-20 08:40:28 -0400

    now summary has full string

--- rtm.rb
+++ rtm.rb
@@ -8,11 +8,16 @@ Dir.foreach(FDIR) do |file|
         #print "Feature file: #{file}\n";
         filepath = FDIR + "/#{file}";
         File.open(filepath).each do |line|
+            line = line.chomp
             if line =~ /^\s*Scenario/ 
-                if line =~ /^\s*Scenario\s*:?(.*?)(\d+\.\d+\.\d+).*$/ 
-                    print $2 + "\t" + $1 + "\t" + "#{file}\n";
+                if line =~ /^\s*Scenario\s*:?(.*?)(\d+\.\d+\.\d+)(.*$)/ 
+                    print $2 + "\t";
+                    print "Scenario: " + $1 + $2 + $3 + "\t";
+                    print file + "\n";
                 elsif line =~ /^\s*Scenario\s*:?(.*)$/ 
-                    print "" + "\t" + $1 + "\t" + "#{file}\n";
+                    print "" + "\t";
+                    print "Scenario: " + $1 + "\t";
+                    print file + "\n";
                 end
             end
         end
