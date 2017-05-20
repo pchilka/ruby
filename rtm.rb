0b2d5ea9e53e83f3268d47f26c890367bf2582f9
Pradeep Chilka <pchilka@gmail.com>
2017-05-20 06:38:38 -0400

    fixed bug to exclude commented Scenarios

--- rtm.rb
+++ rtm.rb
@@ -4,19 +4,18 @@
 FDIR = "features";
 
 Dir.foreach(FDIR) do |file|
-		if file =~ /\.feature$/
-				#print "Feature file: #{file}\n";
-				filepath = FDIR + "/#{file}";
-				File.open(filepath).each do |line|
-					if line =~ /\s*Scenario/ 
-						if line =~ /Scenario:(.*?)(\d+\.\d+\.\d+)\.\d+.*$/ 
-							print $2 + "\t" + $1 + "\t" + "#{file}\n";
-						elsif line =~ /Scenario:(.*)$/ 
-							print "" + "\t" + $1 + "\t" + "#{file}\n";
-
-						end
-					end
-				end
-		end
+    if file =~ /\.feature$/
+        #print "Feature file: #{file}\n";
+        filepath = FDIR + "/#{file}";
+        File.open(filepath).each do |line|
+            if line =~ /^\s*Scenario/ 
+                if line =~ /Scenario:(.*?)(\d+\.\d+\.\d+).*$/ 
+                    print $2 + "\t" + $1 + "\t" + "#{file}\n";
+                elsif line =~ /Scenario:(.*)$/ 
+                    print "" + "\t" + $1 + "\t" + "#{file}\n";
+                end
+            end
+        end
+    end
 end
 
