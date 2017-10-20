300c79f5cd66a087257df23794553048e02c78ed
Pradeep Chilka <pchilka@yahoo.com>
2017-10-20 19:55:43 +0000

    now includes both os and browser

--- census/uaparse.rb
+++ census/uaparse.rb
@@ -1,17 +1,42 @@
 
 require 'user_agent_parser'
 
-Browser         = {};
 OperatingSystem = {};
+
+def fixos(os)
+	if os =~ /^iOS/
+		return "iOS";
+	elsif os =~ /^Mac OS X/
+		return "Mac OS X";
+	elsif os =~ /^Linux/
+		return "Linux";
+	elsif os =~ /^Ubuntu/
+		return "Ubuntu";
+	else
+		return os;
+	end
+	return os;
+end
 
 IO.foreach("splunk.txt") do |line|
     (uastr, count) = line.chomp.split /\t/;
     ua = UserAgentParser.parse uastr
-    #printf "%s, %s, %s\n", ua.family.to_s, ua.version.to_s, ua.os.to_s;
-    b = Browser[ua.family.to_s];
-    Browser[ua.family.to_s] = b.nil? ? count.to_i : b + count.to_i;
+	browser = ua.family.to_s;
+	opersys = fixos(ua.os.to_s);
+	count   = count.to_i;
+	if OperatingSystem[opersys].nil? then
+		OperatingSystem[opersys] = {browser => count};
+	else
+		if OperatingSystem[opersys][browser].nil?
+			OperatingSystem[opersys][browser] = count;
+		else
+			OperatingSystem[opersys][browser] += count;
+		end
+	end
 end
 
-Browser.each do |b,c|
-    printf "%s, %d\n", b, c;
+OperatingSystem.each do |os,browsers|
+	browsers.each do |b,c|
+		printf "%s, %s, %d\n", os, b, c;
+	end
 end
