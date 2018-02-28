41c07b2eecfcadd8f26379b5c34d90173b493122
Pradeep Chilka <pchilka@yahoo.com>
2018-02-28 14:41:09 +0000

    add json parsing

--- wmata/next.rb
+++ wmata/next.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
 require 'json'
+require 'pp'
 
 WMATA_KEY       = "8570cea1dbde47c9a12484beb9bd126d" 
 WMATA_API_URL   = "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/" 
@@ -7,5 +8,5 @@ STATION         = "K08"
 
 request  = WMATA_API_URL + STATION
 headers  = {"api_key" => WMATA_KEY}
-response = RestClient.get(request, headers)
-puts response
+response = JSON.parse RestClient.get(request, headers)
+pp response
